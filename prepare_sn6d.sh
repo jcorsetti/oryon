82118370ffd7be00484db72e2383d79aeb6d9ea9
Jaime Corsetti <jaime.corsetti98@gmail.com>
2024-05-08 12:18:27 +0200

    Fixed script

--- prepare_sn6d.sh
+++ prepare_sn6d.sh
@@ -5,5 +5,5 @@ unzip oryon_data.zip "datasets/shapenet6d/**" -d "${ROOT}/"
 mv ${ROOT}/datasets/shapenet6d/*.json ${ROOT}/
 mv ${ROOT}/datasets/shapenet6d/*.csv ${ROOT}/
 mv ${ROOT}/datasets/shapenet6d/*.pkl ${ROOT}/
-mv "${ROOT}/datasets/nocs/fixed_split" ${ROOT}/
+mv ${ROOT}/datasets/shapenet6d/fixed_split ${ROOT}/
 rm -r "${ROOT}/datasets"
